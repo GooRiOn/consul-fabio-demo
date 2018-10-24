5751511ba9e946f8a933e507144fa9758b669534
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2018-10-24 12:45:02 +0200

    Clean-up

---
+++ src/API/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using API.Services;
+using RestEase;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IMicroservice _microservice;
+
+        //public ValuesController(HostSettings settings)
+        //{
+        //    var host = settings.Services.FirstOrDefault(s => s.ServiceName == "Microservice1")?.HostUrl;
+        //    _microservice = RestClient.For<IMicroservice>(host);
+        //}
+
+        public ValuesController()
+            => _microservice = RestClient.For<IMicroservice>("http://localhost:5001");
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+            => await _microservice.GetValuesAsync();
+    }
+}
---
+++ src/API/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using API.Settings;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var settings = new HostSettings();
+
+            Configuration.GetSection("Host").Bind(settings);
+
+            services.AddSingleton(settings);
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
